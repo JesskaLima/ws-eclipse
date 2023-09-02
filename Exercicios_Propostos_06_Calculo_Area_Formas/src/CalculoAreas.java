6087327e501e2b118b6385d577c5ad841992b039
Jessica L Basilio <jessicalba2006@yahoo.com.br>
2023-09-02 20:46:29 -0300

    Main

--- Exercicios_Propostos_06_Calculo_Area_Formas/src/CalculoAreas.java
+++ Exercicios_Propostos_06_Calculo_Area_Formas/src/CalculoAreas.java
@@ -1,8 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class CalculoAreas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		double c = sc.nextDouble();
+		double pi = 3.14159;
+		
+		double area_triangulo = (a * c) / 2;
+		double area_circulo = pi * Math.pow(c, 2.0);
+		double area_trapezio = ((a + b) * c) / 2;
+		double area_quadrado = Math.pow(b, 2.0);
+		double area_retangulo = a * b;
+		
+		System.out.printf("TRIANGULO: %.3f%nCIRCULO: %.3f%nTRAPEZIO:%.3f%n"
+				+ "QUADRADO: %.3f%nRETANGULO: %.3f%n", area_triangulo, 
+				area_circulo, area_trapezio, area_quadrado, area_retangulo);
+		
+		
 
 	}
 
