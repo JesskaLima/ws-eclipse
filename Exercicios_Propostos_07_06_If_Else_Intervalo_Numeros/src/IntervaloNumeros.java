42341ac5f3a81f49e1a98c14bfb6bc8c65369538
Jessica L Basilio <jessicalba2006@yahoo.com.br>
2023-09-03 15:47:06 -0300

    main

---
+++ Exercicios_Propostos_07_06_If_Else_Intervalo_Numeros/src/IntervaloNumeros.java
@@ -0,0 +1,44 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+/*
+ Você deve fazer um programa que leia um valor qualquer e apresente 
+ uma mensagem dizendo  em qual dos seguintes intervalos 
+ ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. 
+ Obviamente se o valor não estiver em nenhum destes intervalos, 
+ deverá ser impressa a mensagem “Fora de intervalo”.
+ */
+public class IntervaloNumeros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		double x = sc.nextDouble();
+		
+		if (x < 0.0 || x > 100.0) {		
+				System.out.println("Fora do Intervalo");
+		}
+		else if (x >= 75) {
+			System.out.println("Intervalo (75, 100]");
+		}
+		else if (x >= 50) {
+			System.out.println("Intervalo (50, 75]");
+		}
+		else if (x >= 25) {
+			System.out.println("Intervalo (25, 50]");
+		}
+		else {
+			System.out.println("Intervalo (0, 25]");
+		}
+		
+		
+		
+		
+		
+		sc.close();
+		
+
+	}
+
+}
