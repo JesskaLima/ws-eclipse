1d0a2fb35b2e25e14627788868f2fd84e9b6c7a1
Jessica L Basilio <jessicalba2006@yahoo.com.br>
2023-09-03 11:59:53 -0300

    main

--- Exercicios_Propostos_07_04_If_Else_Duracao_Jogo/src/DuracaoJogo.java
+++ Exercicios_Propostos_07_04_If_Else_Duracao_Jogo/src/DuracaoJogo.java
@@ -20,11 +20,15 @@ public class DuracaoJogo {
 			int resultado_final = 0 + hora_final;
 			int duracao_jogo = resultado_inicial + resultado_final;
 			System.out.printf("O jogo durou %d hora(s)", duracao_jogo);
-		} else {
+		} 
+		else if (hora_inicial == 0 && hora_final == 0){
+			System.out.println("O jogo durou 24 horas");
+		} 
+		else {
 			int duracao_jogo = hora_final - hora_inicial;
 			System.out.printf("O jogo durou %d hora(s)", duracao_jogo);
 		}
-
+		
 		sc.close();
 	}
 
