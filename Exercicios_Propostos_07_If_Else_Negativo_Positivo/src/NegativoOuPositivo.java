80e49eb43c402e6a0d3d1fa8f6ca1859fc822773
Jessica L Basilio <jessicalba2006@yahoo.com.br>
2023-09-02 23:19:48 -0300

    main

---
+++ Exercicios_Propostos_07_If_Else_Negativo_Positivo/src/NegativoOuPositivo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NegativoOuPositivo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int numero = sc.nextInt();
+		
+		if (numero >= 0) {
+			System.out.println("Nao negativo");
+		} else {
+			System.out.println("Negativo");
+		}
+		
+		sc.close();
+	}
+
+}
