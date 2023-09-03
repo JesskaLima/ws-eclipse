bf1ca5a917493d8e3ae1bb36ae5eeffcfaf1188c
Jessica L Basilio <jessicalba2006@yahoo.com.br>
2023-09-03 15:52:09 -0300

    main_update

--- Exercicios_Propostos_07_06_If_Else_Intervalo_Numeros/src/IntervaloNumeros.java
+++ Exercicios_Propostos_07_06_If_Else_Intervalo_Numeros/src/IntervaloNumeros.java
@@ -19,13 +19,13 @@ public class IntervaloNumeros {
 		if (x < 0.0 || x > 100.0) {		
 				System.out.println("Fora do Intervalo");
 		}
-		else if (x >= 75) {
+		else if (x >= 75.0) {
 			System.out.println("Intervalo (75, 100]");
 		}
-		else if (x >= 50) {
+		else if (x >= 50.0) {
 			System.out.println("Intervalo (50, 75]");
 		}
-		else if (x >= 25) {
+		else if (x >= 25.0) {
 			System.out.println("Intervalo (25, 50]");
 		}
 		else {
