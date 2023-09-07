2f572bf2cee6bfbc9fb16670c94c9814bd3c0b77
Jessica L Basilio <jessicalba2006@yahoo.com.br>
2023-09-07 16:22:01 -0300

    main
    aula 3

---
+++ estruturadados_algoritmos_java_01_Vetores/src/com/jessica/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,15 @@
+package com.jessica.estruturadados.vetor.teste;
+
+import com.jessica.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		Vetor vetor = new Vetor(5);
+		
+		vetor.adiciona("elemento 1");
+		
+
+	}
+
+}
