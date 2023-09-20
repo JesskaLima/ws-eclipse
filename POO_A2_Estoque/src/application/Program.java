be23ad9c944d8a0e72536e6ae8904855c2ff48f4
Jessica L Basilio <jessicalba2006@yahoo.com.br>
2023-09-19 21:17:11 -0300

    add class & methods

--- POO_A2_Estoque/src/application/Program.java
+++ POO_A2_Estoque/src/application/Program.java
@@ -1,9 +1,35 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product product = new Product();
+		
+		System.out.println("Entre com os dados do produto: ");
+		System.out.print("Nome: ");
+		product.name = sc.next();
+		System.out.print("Price: ");
+		product.price = sc.nextDouble();
+		System.out.print("Quantidade: ");
+		product.quantity = sc.nextInt();
+		 
+		System.out.println(product.name 
+				+ ", " + product.price 
+				+ ", " + product.quantity);
+		
+		
+		
+		
+		
+		sc.close();
 
 	}
 
