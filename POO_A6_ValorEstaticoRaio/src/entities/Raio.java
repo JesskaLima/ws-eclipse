e119dc41e8e3ed42b8cf98ecea71c78d2ded0e38
Jessica L Basilio <jessicalba2006@yahoo.com.br>
2023-09-21 19:08:20 -0300

    Update Raio.java

--- POO_A6_ValorEstaticoRaio/src/entities/Raio.java
+++ POO_A6_ValorEstaticoRaio/src/entities/Raio.java
@@ -1,5 +1,16 @@
 package entities;
 
+/*Fazer um programa para ler um valor numerico qualquer
+ * e daí mostrar quanto seria o valor de uma circunferencia
+ * e do volume de uma esfera para o raio daquele valor.
+ * Informar também o valor de PI com duas casas decimais.
+ * 
+ * exemplo:
+ * Enter radius: 3.0
+ * Circunference: 18.84
+ * Volume: 113.04
+ * PI value: 3.14*/
+
 public class Raio {
 
 }
