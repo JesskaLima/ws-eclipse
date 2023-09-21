dc1a00a1c1c09ed706ea6c66814a25a0293d483a
Jessica L Basilio <jessicalba2006@yahoo.com.br>
2023-09-21 20:07:52 -0300

    metodos

--- POO_A6_ValorEstaticoRaio/src/application/Program.java
+++ POO_A6_ValorEstaticoRaio/src/application/Program.java
@@ -20,18 +20,17 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		Calculator calc = new Calculator();
-		
+				
 		System.out.println("Enter radius: ");
 		double radius = sc.nextDouble();
 		
-		double c = calc.circunference(radius);
+		double c = Calculator.circunference(radius);
 		
-		double v = calc.volume(radius);
+		double v = Calculator.volume(radius);
 		
 		System.out.printf("Circunference: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("PI value: %.2f%n", calc.PI);
+		System.out.printf("PI value: %.2f%n", Calculator.PI);
 				
 		sc.close();
 	}
