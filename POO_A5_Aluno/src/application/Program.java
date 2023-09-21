b239b4910b7732bc77b7c3ec6c3de4f957336fb8
Jessica L Basilio <jessicalba2006@yahoo.com.br>
2023-09-21 17:37:49 -0300

    main

---
+++ POO_A5_Aluno/src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Aluno;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Aluno aluno = new Aluno();
+		
+		System.out.println("Informe o nome do aluno: ");
+		aluno.name = sc.next();
+		System.out.println("Informe as 3 notas do aluno: ");
+		aluno.nota1 = sc.nextDouble();
+		aluno.nota2 = sc.nextDouble();
+		aluno.nota3 = sc.nextDouble();
+		
+		System.out.printf("Nota final: %.2f%n", aluno.somarNota());
+		
+		if (aluno.somarNota() < 60) {
+			System.out.println("Failed");
+			System.out.printf("Missing %.2f points", aluno.notaRestante());			
+		}
+		else {
+			System.out.println("Pass");
+		}
+		
+		
+		
+		sc.close();
+
+	}
+
+}
