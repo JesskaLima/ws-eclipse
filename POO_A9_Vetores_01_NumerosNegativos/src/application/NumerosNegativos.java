26608d9631e9bc364cf5b61c0574941abec3f529
Jessica L Basilio <jessicalba2006@yahoo.com.br>
2023-09-26 18:30:58 -0300

    update

---
+++ POO_A9_Vetores_01_NumerosNegativos/src/application/NumerosNegativos.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+/*Faça um programa que leia um número inteiro positivo N (máximo = 10) e depois 
+ * N números inteiros e armazene-os em um vetor. 
+ * Em seguida, mostrar na tela todos os números negativos lidos*/
+public class NumerosNegativos {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Quantos numeros voce vai digitar? ");
+		int numeros = sc.nextInt() ;
+		int vetor[] = new int[numeros];
+		
+		for (int i = 0; i < vetor.length; i ++) {
+			System.out.println("Digite um numero: ");
+			vetor[i] =  sc.nextInt();
+			}
+		for (int i = 0; i < vetor.length; i ++) {
+			if (vetor[i] < 0) {
+				System.out.println("NUMEROS NEGATIVOS: " + vetor[i]);
+			}
+		}
+	
+		
+		sc.close();
+
+	}
+
+}
